cace50462b6370300ff4eb4c0f7aab8c9cd22faf
Univerce <505772146@qq.com>
2019-02-11 09:04:18 +0800

    Update 3.go

--- 3.go
+++ 3.go
@@ -1,1 +1,7 @@
 Go是一门类似C的编译型语言，但是它的编译速度非常快。
+
+break    default      func    interface    select
+case     defer        go      map          struct
+chan     else         goto    package      switch
+const    fallthrough  if      range        type
+continue for          import  return       var
