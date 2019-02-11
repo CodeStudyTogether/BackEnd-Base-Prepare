fff2304eeaf1146a6c3993f4837380e618a7da0d
Univerce <505772146@qq.com>
2019-02-11 09:09:00 +0800

    Update 3.go

--- 3.go
+++ 3.go
@@ -5,3 +5,14 @@ case     defer        go      map          struct
 chan     else         goto    package      switch
 const    fallthrough  if      range        type
 continue for          import  return       var
+
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world or 你好，世界 or καλημ ́ρα κóσμ or こんにちはせかい\n")
+}
+
+main.main()函数(这个函数位于主包）是每一个独立的可运行程序的入口点。Go使用UTF-8字符串和标识符(因为UTF-8的发明者也就是Go的发明者之一)，所以它天生支持多语言。
