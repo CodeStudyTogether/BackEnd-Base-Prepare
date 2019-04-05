ccc2348c5dd0ec455dff69cdde45275cc334631f
Univerce <505772146@qq.com>
2019-04-05 16:07:29 +0800

    Update 4.java

--- 4.java
+++ 4.java
@@ -1,1 +1,9 @@
 这些框架比较依赖的是Java的反射和注解机制
+
+JavaEE框架，从百家混战到现在Spring基本一统天下。
+
+Web开发，从标配的SSH到现在SpirngMVC + MyBatis组合。
+
+IDE，从当年如火如荼的JBuilder到Eclipse，再到更好用的IDEA。
+
+当前，Servlet虽然不再是一个主流web开发技术，但依然是Java Web开发技术的基础，是Java Web容器的基石，是行业标准。而现在流行的各种MVC框架（包括SpringMVC），在最底层，还是以 Servlet为基础的。
