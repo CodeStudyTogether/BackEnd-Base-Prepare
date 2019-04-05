4a6c322955be1b823b1aff9bfa78bca6258ca04b
Univerce <505772146@qq.com>
2019-04-05 15:09:17 +0800

    Update 2.java

--- 2.java
+++ 2.java
@@ -1,2 +1,6 @@
 当该类有父类的时候，类的实例化的加载过程是：
 父类的static成员变量和方法》该类的static变量和方法》开始实例化》父类的普通成员变量和方法》父类的构造方法》该类的普通成员变量和方法》该类的构造方法》实例化结束。
+
+实际上后台开发人员通常就是写个Controller 提供个 Model 给你。而 Hibernate 和 MyBatis 都是持久层框架，属于能方便使用如 MySql 等数据库的 ORM 框架。
+
+所以现阶段而言，个人认为 Spring、SpringMVC、SpringBoot 是 Android 开发者学习入门 Java Web 的不错选择。
