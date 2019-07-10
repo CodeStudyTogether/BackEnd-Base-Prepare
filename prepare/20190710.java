3b7edbda4722d85b9cbb004d7fb51e5f27d5ff98
Univerce <505772146@qq.com>
2019-07-10 16:14:17 +0800

    Update 20190710.java

--- prepare/20190710.java
+++ prepare/20190710.java
@@ -1,1 +1,30 @@
+插入排序：
+public static void insertSort(int[] a) {
+        int i, j, insertNote;// 要插入的数据
+        for (i = 1; i < a.length; i++) {// 从数组的第二个元素开始循环将数组中的元素插入
+            insertNote = a[i];// 设置数组中的第2个元素为第一次循环要插入的数据
+            j = i - 1;
+            while (j >= 0 && insertNote < a[j]) {
+                a[j + 1] = a[j];// 如果要插入的元素小于第j个元素,就将第j个元素向后移动
+                j--;
+            }
+            a[j + 1] = insertNote;// 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
+        }
+    }
 
+选择排序：
+    for (int i=0;i<arr.length;i++){
+        //临时变量
+        int temp;
+        for (int j=i+1;j<arr.length;j++){
+            //若发现被比较的元素小于选择的元素，交换两个的值
+            if (arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    return arr;
+}
+给定数组：int[] arr={里面n个数据}；第1趟排序，在待排序数据arr[1]~arr[n]中选出最小的数据，将它与arrr[1]交换；第2趟，在待排序数据arr[2]~arr[n]中选出最小的数据，将它与r[2]交换；以此类推，第i趟在待排序数据arr[i]~arr[n]中选出最小的数据，将它与r[i]交换，直到全部排序完成。
